06a5165b92959cd2587c66c0c34d11ffc42158dc
Richard Palacios G <apalaciosg91@gmail.com>
2021-05-04 23:48:46 -0500

    feat(routing): add and configure routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//Rutas
 import { AppRoutingModule } from './app-routing.module';
+//Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
