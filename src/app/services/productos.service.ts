1dcfa2e07a5b53512b56b6a247027c8c67f90344
Richard Palacios G <apalaciosg91@gmail.com>
2021-05-05 09:27:05 -0500

    feat(search): add logic to process load and filter of search component

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -17,11 +17,16 @@ export class ProductosService {
   }
 
   private cargarProductos() {
-    this.http.get<Producto[]>('https://html-to-angular-c904d-default-rtdb.firebaseio.com/productos_idx.json')
-      .subscribe((resp: Producto[]) => {
-        this.productos = resp;
-        this.cargando = false;
-      });
+
+    return new Promise((resolve: any, reject) => {
+      this.http.get<Producto[]>('https://html-to-angular-c904d-default-rtdb.firebaseio.com/productos_idx.json')
+        .subscribe((resp: Producto[]) => {
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
+        });
+    });
+
   }
 
   getProducto(id: string) {
@@ -29,9 +34,34 @@ export class ProductosService {
   }
 
   buscarProducto(termino: string) {
-    this.productosFiltrados = this.productos.filter(producto => {
-      return true;
+
+    if (this.productos.length === 0) {
+      //cargar productos
+      this.cargarProductos().then(() => {
+        // ejecutar despues de tener los productos
+        //Aplicar filtro
+        this.filtrarProductos(termino);
+      });
+    } else {
+      //aplicar el filtro
+      this.filtrarProductos(termino);
+    }
+    // this.productosFiltrados = this.productos.filter(producto => {
+    //   return true;
+    // });
+    // console.log(this.productosFiltrados);
+  }
+
+  private filtrarProductos(termino: string) {
+    this.productosFiltrados = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+      const tituloLower = prod.titulo.toLowerCase();
+      if (prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrados.push(prod);
+      }
     });
-    console.log(this.productosFiltrados);
   }
 }
