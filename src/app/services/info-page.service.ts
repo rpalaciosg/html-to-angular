1bf57f7c952bd2c8baf5342e2ebcf4b672ea878b
Richard Palacios G <apalaciosg91@gmail.com>
2021-05-05 00:59:03 -0500

    feat(service): create interface infoPage to use in services response object type

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { infoPagina } from '../interfaces/info-age.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPageService {
 
-  info: any = {};
+  info: infoPagina = {};
   cargada: boolean = false;
 
   constructor(private http: HttpClient) {
@@ -15,7 +16,7 @@ export class InfoPageService {
     //Leer archivo JSON src/assets/data/data-page.json con httpClient
     // Aqui puedo consumir sevricios Rest
     this.http.get('assets/data/data-page.json')
-      .subscribe(resp => {
+      .subscribe((resp: infoPagina) => {
         this.cargada = true;
         this.info = resp;
         console.log(resp);
