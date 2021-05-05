1b5399db093faaf41b83ef17d211dadbfcc2d46c
Richard Palacios G <apalaciosg91@gmail.com>
2021-05-05 02:25:21 -0500

    feat(service): add cargarEquiupo to consume rest from firebase

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -9,16 +9,27 @@ export class InfoPageService {
 
   info: infoPagina = {};
   cargada: boolean = false;
+  equipo: any = [];
 
   constructor(private http: HttpClient) {
-    // console.log('Servicio de infoPage listo..!');
 
-    //Leer archivo JSON src/assets/data/data-page.json con httpClient
-    // Aqui puedo consumir sevricios Rest
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-page.json')
       .subscribe((resp: infoPagina) => {
         this.cargada = true;
         this.info = resp;
+        // console.log(resp);
+      });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://html-to-angular-c904d-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe(resp => {
+        this.equipo = resp;
         console.log(resp);
       });
   }
